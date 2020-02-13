f16e366f39436f939f6b71aea5a95cebf5f08248
ivan <Ivan.krivoruchko.1999@gmail.com>
2020-02-13 10:42:26 +0200

    hw spring intro

---
+++ src/main/java/spring/intro/service/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package spring.intro.service;
+
+import spring.intro.dao.UserDao;
+import spring.intro.model.User;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    @Qualifier(value = "userDaoImpl")
+    private UserDao userDao;
+
+    @Override
+    public User add(User user) {
+        return userDao.add(user);
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return userDao.findByEmail(email);
+    }
+
+    @Override
+    public List<User> getAll() {
+        return userDao.getAll();
+    }
+}
